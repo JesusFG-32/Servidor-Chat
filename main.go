31907957f47dbed53e9a2b8cb965429faa9a94b8
Jesús Antonio Flores González <jesusfloresglz12@outlook.com>
2026-02-21 17:32:00 -0600

    Arreglos de rutas

--- main.go
+++ main.go
@@ -34,23 +34,78 @@ func main() {
 	http.HandleFunc("/app/chat/api/logout", LogoutHandler)
 	http.HandleFunc("/app/chat/api/session", SessionHandler)
 
+	http.HandleFunc("/api/register", RegisterHandler)
+	http.HandleFunc("/api/login", LoginHandler)
+	http.HandleFunc("/api/logout", LogoutHandler)
+	http.HandleFunc("/api/session", SessionHandler)
+
+	http.HandleFunc("/register", RegisterHandler)
+	http.HandleFunc("/login", LoginHandler)
+	http.HandleFunc("/logout", LogoutHandler)
+	http.HandleFunc("/session", SessionHandler)
+
 	http.HandleFunc("/app/chat/ws", func(w http.ResponseWriter, r *http.Request) {
+		ServeWs(hub, w, r)
+	})
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		ServeWs(hub, w, r)
 	})
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		urlPath := r.URL.Path
+		basePath := "/"
+
+		if len(urlPath) >= 4 && urlPath[:4] == "/api" || len(urlPath) >= 13 && urlPath[:13] == "/app/chat/api" {
+			http.Error(w, "API Route Not Found: "+urlPath, http.StatusNotFound)
+			return
+		}
+
 		if len(urlPath) >= 9 && urlPath[:9] == "/app/chat" {
+			basePath = "/app/chat"
 			urlPath = urlPath[9:]
 			if urlPath == "" {
 				urlPath = "/"
 			}
 		}
+
+		if urlPath == "/ws" || urlPath == "/ws/" {
+			ServeWs(hub, w, r)
+			return
+		}
+
+		isValidSession, _ := ValidateSession(r)
+		/*
+			if urlPath == "/room" && !isValidSession {
+				redirectTarget := basePath
+				if redirectTarget == "/app/chat" {
+					redirectTarget += "/"
+				}
+				log.Println("Redirecting user to " + redirectTarget + " from /room (l1)")
+
+				log.Println("Redirecting user to /app/chat/room from /room (l2)")
+				return
+			}
+		*/
+
+		if urlPath == "/" && isValidSession {
+			redirectTarget := basePath
+			if redirectTarget == "/" {
+				redirectTarget = "/room"
+			} else {
+				redirectTarget = basePath + "/room"
+			}
+			log.Println("Redirecting user to " + redirectTarget + " from / (l3)")
+			http.Redirect(w, r, redirectTarget, http.StatusFound)
+			return
+		}
+
 		path := "./public" + urlPath
 		if _, err := os.Stat(path); os.IsNotExist(err) && urlPath != "/" {
 			http.ServeFile(w, r, "./public/index.html")
 			return
 		}
+
 		r.URL.Path = urlPath
 		fs.ServeHTTP(w, r)
 	})
